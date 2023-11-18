c04fed15dbb1c9f8dab4c82461c228621acd6eaa
BANADDA <banaddamubaraka68@gmail.com>
2023-11-18 11:45:16 +0300

    modified:   digi_save_vsla_api/views/__pycache__/auth_view.cpython-312.pyc 	modified:   digi_save_vsla_api/views/auth_view.py

--- digi_save_vsla_api/views/auth_view.py
+++ digi_save_vsla_api/views/auth_view.py
@@ -36,35 +36,69 @@ def login_with_phone_unique_code(request):
                     'message': str(e),
                   }, status=500)
 
+                is_admin = user.is_staff
 
+                if is_admin:
 
-                response_data = {
-                    "status": status.HTTP_200_OK,
-                    'success': True,
-                    "Token": token.key if token else None,
-                    'user': {
-                        'id': user.id,
-                        'fname': user.fname,
-                        'lname': user.lname,
-                        'email': user.email,
-                        'image': user.image,
-                        'unique_code':user.unique_code,
-                        'phone': user.phone,
-                        'sex': user.sex,
-                        'country': user.country,
-                        'date_of_birth': user.date_of_birth,
-                        'district': user.district,
-                        'subCounty': user.subCounty,
-                        'village': user.village,
-                        'number_of_dependents': user.number_of_dependents,
-                        'family_information': user.family_information,
-                        'next_of_kin_name': user.next_of_kin_name,
-                        'next_of_kin_has_phone_number': user.next_of_kin_has_phone_number,
-                        'next_of_kin_phone_number': user.next_of_kin_phone_number,
-                        'pwd_type': user.pwd_type,
-                    },
-                }
-                return JsonResponse(response_data, status=status.HTTP_200_OK)
+                    response_data = {
+                        "status": status.HTTP_200_OK,
+                        'success': True,
+                        "Token": token.key if token else None,
+                        "is_admin": True,
+                        'user': {
+                            'id': user.id,
+                            'fname': user.fname,
+                            'lname': user.lname,
+                            'email': user.email,
+                            'image': user.image,
+                            'unique_code':user.unique_code,
+                            'phone': user.phone,
+                            'sex': user.sex,
+                            'country': user.country,
+                            'date_of_birth': user.date_of_birth,
+                            'district': user.district,
+                            'subCounty': user.subCounty,
+                            'village': user.village,
+                            'number_of_dependents': user.number_of_dependents,
+                            'family_information': user.family_information,
+                            'next_of_kin_name': user.next_of_kin_name,
+                            'next_of_kin_has_phone_number': user.next_of_kin_has_phone_number,
+                            'next_of_kin_phone_number': user.next_of_kin_phone_number,
+                            'pwd_type': user.pwd_type,
+                        },
+                    }
+                    return JsonResponse(response_data, status=status.HTTP_200_OK)
+                
+                else:
+
+                    response_data = {
+                        "status": status.HTTP_200_OK,
+                        'success': True,
+                        "Token": token.key if token else None,
+                        "is_admin": False,
+                        'user': {
+                            'id': user.id,
+                            'fname': user.fname,
+                            'lname': user.lname,
+                            'email': user.email,
+                            'image': user.image,
+                            'unique_code':user.unique_code,
+                            'phone': user.phone,
+                            'sex': user.sex,
+                            'country': user.country,
+                            'date_of_birth': user.date_of_birth,
+                            'district': user.district,
+                            'subCounty': user.subCounty,
+                            'village': user.village,
+                            'number_of_dependents': user.number_of_dependents,
+                            'family_information': user.family_information,
+                            'next_of_kin_name': user.next_of_kin_name,
+                            'next_of_kin_has_phone_number': user.next_of_kin_has_phone_number,
+                            'next_of_kin_phone_number': user.next_of_kin_phone_number,
+                            'pwd_type': user.pwd_type,
+                        },
+                    }
+                    return JsonResponse(response_data, status=status.HTTP_200_OK)
             
             else:
                 response = {
