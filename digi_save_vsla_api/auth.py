4a2c8f0b110d8c2bb19f5f65752e9d92f224b751
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2023-12-02 12:20:00 +0300

    Update auth.py

--- digi_save_vsla_api/auth.py
+++ digi_save_vsla_api/auth.py
@@ -2,12 +2,22 @@ from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
 
+# Updated auth backend
 class PhoneCodeBackend(BaseBackend):
     def authenticate(self, request, phone=None, unique_code=None, **kwargs):
         User = get_user_model()
         try:
             user = User.objects.get(phone=phone)
-            if check_password(unique_code, user.unique_code):
+            print(f"User found with phone number: {user.phone}")
+            
+            stored_hashed_password = user.unique_code
+            print(f"Stored Hashed Password: {stored_hashed_password}")  # Debug line
+            
+            if check_password(unique_code, stored_hashed_password):
+                print("Password matched")
                 return user
+            else:
+                print("Password did not match")
         except User.DoesNotExist:
+            print("User does not exist")
             return None
